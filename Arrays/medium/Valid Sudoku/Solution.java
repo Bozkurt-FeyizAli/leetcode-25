54f47afdabcb08f1f670bccc40ee9e092c95db0f
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-09-26 16:08:20 +0300

    isValidSudoku v2

--- Arrays/medium/Valid Sudoku/Solution.java
+++ Arrays/medium/Valid Sudoku/Solution.java
@@ -4,12 +4,12 @@ import java.util.Set;
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int sum='1'+'2'+'3'+'4'+'5'+'6'+'7'+'8'+'9';
-        for (int i = 0; i < board.length; i++) {
-            if(sumRow(board, i)!=sum) return false;
-        }
-        for (int i = 0; i < board.length; i++) {
-            if(sumColumn(board, i)!=sum) return false;
-        }
+        // for (int i = 0; i < board.length; i++) {
+        //     if(sumRow(board, i)!=sum) return false;
+        // }
+        // for (int i = 0; i < board.length; i++) {
+        //     if(sumColumn(board, i)!=sum) return false;
+        // }
         for (int i = 0; i < board.length; i++) {
             if(hasDuplicateRow(board, i)) return false;
         }
@@ -19,20 +19,20 @@ class Solution {
         return true;
     }
 
-    public static int sumRow(char[][] row, int n){
-        int res=0;
-        for (char c : row[n]) {
-            res+=c;
-        }
-        return res;
-    }
-    public static int sumColumn(char[][] row, int n){
-        int res=0;
-        for (int i = 0; i < row.length; i++) {
-            res+=row[i][n];
-        }
-        return res;
-    }
+    // public static int sumRow(char[][] row, int n){
+    //     int res=0;
+    //     for (char c : row[n]) {
+    //         res+=c;
+    //     }
+    //     return res;
+    // }
+    // public static int sumColumn(char[][] row, int n){
+    //     int res=0;
+    //     for (int i = 0; i < row.length; i++) {
+    //         res+=row[i][n];
+    //     }
+    //     return res;
+    // }
 
     public static boolean hasDuplicateRow(char[][] row, int n){
         Set<Character> set= new HashSet<>();
@@ -54,4 +54,18 @@ class Solution {
         }
         return l!=set.size();
     }
+
+    public static boolean hasDuplicate9(char[][] arr){
+        Set<Character> set= new HashSet<>();
+        int l=0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                char c = arr[i][j];
+                if(c != '.'){set.add(c);
+                l++;}
+            }
+        }
+        return l!=set.size();
+    }
+    
 }
