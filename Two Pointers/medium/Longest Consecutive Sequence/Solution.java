810bf249c7d3a3e7f245dca459e627d01fc19d73
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-09-26 22:30:45 +0300

    longestConsecutive v1

---
+++ Two Pointers/medium/Longest Consecutive Sequence/Solution.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> map= new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.add(nums[i]);
+        }
+        int res=0;
+        HashSet<Integer> visited= new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n=nums[i];
+            if(map.contains(n-1)||visited.contains(n)) continue;
+            int l=1;
+            visited.add(n);
+            while(map.contains(n+1)){
+                l++;
+                n=n+1;
+            }
+            if(l>res){
+                res=l;
+            }
+        }
+        return res;
+    }
+}
+
